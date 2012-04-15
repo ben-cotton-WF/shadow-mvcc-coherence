292c36a5f92ac60955524195468eb44c12fb4d75
david <david@6bb6abf1-44d3-0a69-b801-943b62118d59>
2012-04-15 20:40:33 +0000

    license headers

--- src/main/java/com/shadowmvcc/coherence/index/MVCCIndex.java
+++ src/main/java/com/shadowmvcc/coherence/index/MVCCIndex.java
@@ -1,3 +1,25 @@
+/*
+
+Copyright 2012 Shadowmist Ltd.
+
+This file is part of Shadow MVCC for Oracle Coherence.
+
+Shadow MVCC for Oracle Coherence is free software: you can redistribute 
+it and/or modify it under the terms of the GNU General Public License 
+as published by the Free Software Foundation, either version 3 of the 
+License, or (at your option) any later version.
+
+Shadow MVCC for Oracle Coherence is distributed in the hope that it 
+will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See 
+the GNU General Public License for more details.
+                        
+You should have received a copy of the GNU General Public License
+along with Shadow MVCC for Oracle Coherence.  If not, see 
+<http://www.gnu.org/licenses/>.
+
+*/
+
 package com.shadowmvcc.coherence.index;
 
 import java.util.Comparator;
